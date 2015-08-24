e5dea4d13968c724d93d8a042d39e1ead942cd85
Alex <96dalex@gmail.com>
2015-08-23 20:42:20 -0600

    phone number i2, exercism renamed file

--- phone-number/phone_number.js
+++ phone-number/phone_number.js
@@ -1,6 +1,5 @@
 function PhoneNumber(number) {
-  this._number = number.replace(/[^0-9]/g, '');
-  this.cleanNumber();
+  this.parseNumber(number);
 }
 
 PhoneNumber.prototype.number = function() {
@@ -11,11 +10,20 @@ PhoneNumber.prototype.areaCode = function() {
   return this.number().slice(0, 3);
 };
 
-PhoneNumber.prototype.toString = function () {
-  return '(' + this.areaCode() + ') ' + this.number().slice(3, 6) + '-' + this.number().slice(6, 10);
+PhoneNumber.prototype.prefix = function() {
+  return this.number().slice(3, 6);
 };
 
-PhoneNumber.prototype.cleanNumber = function() {
+PhoneNumber.prototype.suffix = function() {
+  return this.number().slice(6, 10);
+};
+
+PhoneNumber.prototype.toString = function () {
+  return '(' + this.areaCode() + ') ' + this.prefix() + '-' + this.suffix();
+};
+
+PhoneNumber.prototype.parseNumber = function(rawNumber) {
+  this._number = rawNumber.replace(/[^0-9]/g, '');
   if(this._number.length == 11 && this._number[0] == '1') {
     this._number = this._number.slice(1);
   } else if(this._number.length != 10) {
